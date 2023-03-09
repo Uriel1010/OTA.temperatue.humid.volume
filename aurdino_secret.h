28246e81959585b2227110e4ae8ffcfd47675a19
Uriel1010 <66363420+Uriel1010@users.noreply.github.com>
2023-03-09 12:49:36 +0200

    Delete aurdino_secret.h

--- aurdino_secret.h
+++
@@ -1,6 +0,0 @@
-#define SSID_home "Manzur"
-#define SSID_PASS "089923743"
-#define mqttServer_add = "192.168.31.130"; // The IP address of the MQTT broker
-#define mqttPort_num = 1883; // The port number of the MQTT broker
-#define mqttUsername = "home"; // The username for connecting to the MQTT broker
-#define mqttPassword_p = "089923743"; // The password for connecting to the MQTT broker
